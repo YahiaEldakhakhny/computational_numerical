9534f415d0b60e098e26d5b11f6a662edf9a18b1
YahiaEldakhakhny <yahia4360673@gmail.com>
2022-05-20 12:55:22 +0200

    make trapezoid more user friendly

--- Trapezoid.m
+++ Trapezoid.m
@@ -1,17 +1,17 @@
 a = input("Enter the first value:\n");
 b = input("Enter the last value:\n");
-n = input ('Enter the number of sections :\n');
+n = input ("Enter the number of sections :\n");
 x = linspace(a,b,n+1);
 
 str = input('Give an equation in x: ','s') ;
 f = str2func(['@(x)',(str)]) ;
 y = f(x);
 
-delta = ( x(length((x))-x(1)) )/n
+delta = ( x(length((x))-x(1)) )/n;
 Area = 0;
-Area = y(1) + y(length(x)) %Adding the the first and last values f(a), f(b)
+Area = y(1) + y(length(x)); %Adding the the first and last values f(a), f(b)
 %Adding twice the middle heights to the area.
 for i= 2:length(x)-1
-    Area = Area + 2*y(i)
+    Area = Area + 2*y(i);
 end
 Area = Area*delta/2 %The area is multiplied by the step size divided by 2
